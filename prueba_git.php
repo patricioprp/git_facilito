f9821059391a5bc59638c45b9556823e7505e9fb
Patricio <patricioprp06@gmail.com>
2017-10-14 22:05:06 -0300

    se cambio el tamaño

--- prueba_git.php
+++ prueba_git.php
@@ -15,7 +15,7 @@
                 padding: 0;
                 width: 100%;
                 display: table;
-                font-weight: 100;
+                font-weight: 200;
                 font-family: 'Lato';
             }
 
