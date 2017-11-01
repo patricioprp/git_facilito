73e23970822ec7bb7d86887beeba2cc414c41433
Patricio <patricioprp06@gmail.com>
2017-11-01 00:07:42 -0300

    se agrega linea para saber en que rama estamos

--- prueba_git.php
+++ prueba_git.php
@@ -43,7 +43,7 @@
                 <div class="title">Laravel 5</div>
         <p> <b>lorem lorem lorem</b></p>
   <strong>  <p>ESTA LINEA ES EL PRIMER CAMBIO QUE HAY EN EL ARCHIVO...</p><strong>
-  <strong>  <p>este es un cambio realizado desde la rama master</p></strong>
+  <strong>  <p>este es un cambio realizado desde la rama test</p></strong>
     <button type="button" class="btn btn-info">autodestruccion</button>
             </div>
         </div>
